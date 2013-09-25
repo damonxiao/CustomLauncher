7898a74d12a7b98c79bfe336a7ca8d234e5cddc5
--damon <--qukers@126.com>
2013-09-25 16:55:48 +0800

    Change asset file get automantic

--- CustomLauncher/src/com/seuic/launcher/Albums.java
+++ CustomLauncher/src/com/seuic/launcher/Albums.java
@@ -2,22 +2,19 @@ package com.seuic.launcher;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.widget.ImageSwitcher;
 
+import com.seuic.launcher.util.Const;
 import com.seuic.launcher.widget.AlbumAdapter;
 import com.seuic.launcher.widget.AlbumGallery;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Albums extends Activity {
     private static final List<String> IMAGES = new ArrayList<String>();
-    static{
-        IMAGES.add("albums/001.jpg");
-        IMAGES.add("albums/002.jpg");
-        IMAGES.add("albums/003.jpg");
-    }
     
     private AlbumGallery mGallery;
     
@@ -27,6 +24,14 @@ public class Albums extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album);
+        try {
+            String files[] = getAssets().list(Const.ASSET_IMAGE_BASE_PATH);
+            for(String file:files){
+                IMAGES.add(Const.ASSET_IMAGE_BASE_PATH+File.separator+file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mGallery = (AlbumGallery) findViewById(R.id.album_gallery);
         mAdapter = new AlbumAdapter(IMAGES, this);
         mGallery.setAdapter(mAdapter);
--- CustomLauncher/src/com/seuic/launcher/util/Const.java
+++ CustomLauncher/src/com/seuic/launcher/util/Const.java
@@ -2,4 +2,5 @@ package com.seuic.launcher.util;
 
 public class Const {
     public static final String EXTRA_PACKAGE_NAME = "EXTRA_APP_INFO";
+    public static final String ASSET_IMAGE_BASE_PATH = "albums";
 }
